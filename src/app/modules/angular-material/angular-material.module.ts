029b978e1ec1b0fa52b02a3d94fd44c5bfa87d3a
Damir <damir8520@gmail.com>
2020-04-21 12:43:19 +0100

    created an angular-material moduel to hold all of the angular-material features.
    imported to shared module for use across the app.

---
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from "@angular/material/card";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatInputModule } from "@angular/material/input";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatListModule } from "@angular/material/list";
+import { MatDialogModule } from "@angular/material/dialog";
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatListModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatDialogModule,
+
+  ]
+})
+export class AngularMaterialModule { 
+
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,22 +8,13 @@ import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { LoginComponent } from "../auth/login/login.component";
 import { RegisterComponent } from "../auth/register/register.component";
 
-import { MatCardModule } from "@angular/material/card";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatInputModule } from "@angular/material/input";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatListModule } from "@angular/material/list";
-import { MatDialogModule } from "@angular/material/dialog";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { FormsModule } from "@angular/forms";
 import { CardComponent } from "./widgets/card/card.component";
 import { DashboardComponent } from "../modules/dashboard/dashboard.component";
 import { ErrorComponent } from "../error/error/error.component";
+import { AngularMaterialModule } from "../modules/angular-material/angular-material.module";
 
 @NgModule({
   declarations: [
@@ -40,17 +31,8 @@ import { ErrorComponent } from "../error/error/error.component";
     CommonModule,
     HttpClientModule,
     RouterModule,
-    MatDividerModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
+    AngularMaterialModule,
     FlexLayoutModule,
-    MatMenuModule,
-    MatListModule,
-    MatInputModule,
-    MatProgressSpinnerModule,
-    MatCardModule,
-    MatDialogModule,
     FormsModule,
   ],
   exports: [
