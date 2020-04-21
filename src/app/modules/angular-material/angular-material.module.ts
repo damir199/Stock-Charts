2cd3e023f5e6d86b1d5c2e0dde54a9ca1570e087
Damir <damir8520@gmail.com>
2020-04-21 15:03:24 +0100

    small design changes on error component.

--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatCardModule } from "@angular/material/card";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatInputModule } from "@angular/material/input";
