1065ce45fd58250bee3a2b747802b888ab372516
Damir <damir8520@gmail.com>
2020-05-13 20:28:34 +0100

    fixed the display issue with the divs on the sales component.

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -10,7 +10,7 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   searchOption=[]
-  public postsData: IPost[] 
+  public postsData: IPost[]
 
   getPost(id: string){
     return {...this.postsData.find(p => p.id == id)}
@@ -23,7 +23,7 @@ export class PostService {
   filteredListOptions() {
     let posts = this.postsData;
         let filteredPostsList = [];
-    
+
         for (let post of posts) {
             for (let options of this.searchOption) {
                 if (options.title === post.title) {
